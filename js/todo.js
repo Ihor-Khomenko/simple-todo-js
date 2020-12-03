b382aa6e28f6de5375e084d925be2bfee9b81a8f
Ihor Khomenko <ikhomenko.it@gmail.com>
2020-12-03 09:50:23 -0500

    edit button functionality

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,4 @@
+//Create DOM element
 function createTodoItem(title) {
     const checkbox = document.createElement('input');
     //change attribute type and set className
@@ -16,14 +17,15 @@ function createTodoItem(title) {
 
     const editButton = document.createElement('button');
     //Set inner text for edit button
-    editButton.innterText = 'Edit';
+    editButton.innerText = 'Edit';
     editButton.className = 'edit';
 
     const deleteButton = document.createElement('button');
     //Set inner text for delete button
-    deleteButton.innterText = 'Delete';
+    deleteButton.innerText = 'Delete';
     deleteButton.className = 'delete';
 
+    //Create li element
     const listItem = document.createElement('li');
     listItem.className = 'todo-item';
 
@@ -35,17 +37,61 @@ function createTodoItem(title) {
     listItem.appendChild(deleteButton);
     console.log(listItem);
 
+    bindEvents(listItem); //Subscribe to events Bind items
+
     return listItem;
 }
 
+//Bind Events
+function bindEvents(todoItem) {
+    const checkbox = todoItem.querySelector('.checkbox'); //gain access to DOM elements
+    const editButton = todoItem.querySelector('button.edit');
+    const deleteButton = todoItem.querySelector('button.delete');
+
+    checkbox.addEventListener('change', toggleTodoItem);
+    editButton.addEventListener('click', editTodoItem);
+    deleteButton.addEventListener('click', deleteTodoItem);
+}
 
 function addTodoItem(event) {
     event.preventDefault(); //Stop send to server. Don't reload page when submitted
 
     //if empty string submitted show error
-    if(addInput.value === '') return alert('Please, enter the name for the task');
-    const listItem = createTodoItem(addInput.value);
+    if(addInput.value === '') {
+        return alert('Please, enter the name for the task');
+    }
+        
+    
+    const todoItem = createTodoItem(addInput.value);
+    todoList.appendChild(todoItem);
+    addInput.value = ''; //make string empty
 }
+
+
+function toggleTodoItem() {
+    const listItem = this.parentNode;
+    listItem.classList.toggle('completed');
+}
+
+function editTodoItem() {
+    const listItem = this.parentNode;
+    const title = listItem.querySelector('.title');
+    const editInput = listItem.querySelector('.textfield');
+    const isEditing = listItem.classList.contains('editing');
+
+    if (isEditing) {
+        title.innerText = editInput.value; //assign value from edit field to innertext
+        this.innerText = 'Edit';
+    } else {
+        editInput.value = title.innerText;
+        this.innerText = 'Save';
+    }
+    listItem.classList.toggle('editing');
+}
+
+function deleteTodoItem() {
+
+};
 
 const todoForm = document.getElementById('todo-form');
 const addInput = document.getElementById('add-input');
