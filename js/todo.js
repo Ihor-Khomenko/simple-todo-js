935c82740d21a29692b600e83faf7e3eb0150430
Ihor Khomenko <ikhomenko.it@gmail.com>
2020-12-03 08:39:17 -0500

    Initial project version

---
+++ js/todo.js
@@ -0,0 +1,5 @@
+const todoForm = document.getElementById('todo-form');
+const addInput = document.getElementById('add-input');
+const todoList = document.getElementById('todo-list');
+const todoItems = document.querySelectorAll('.todo-item');
+
